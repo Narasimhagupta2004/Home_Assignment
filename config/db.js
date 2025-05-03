44c0bcc59d6e398f4a9b5ac56f23a3d56d6e01b5
BANDARU NARASIMHAGUPTA <115392495+Narasimhagupta2004@users.noreply.github.com>
2025-05-03 10:28:56 +0530

    Update db.js
    comments added

--- config/db.js
+++ config/db.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
 
-// Function to connect to MongoDB database
+// Establish a connection to the MongoDB database
 const connectDB = async () => {
   try {
-    // Attempt to connect to MongoDB using the URI from environment variables
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    // Log a successful connection with the MongoDB host
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } 
-  catch (err) {
-    // Log an error message if the connection fails
+  } catch (err) {
     console.error(`MongoDB Connection Error: ${err.message}`);
-    // Exit the process if the database connection fails
-    process.exit(1);  // Exits with a non-zero status to indicate failure
+    process.exit(1);  // Exit process with failure
   }
 };
 
-// Export the connection function to use it in other parts of the application
 module.exports = connectDB;
